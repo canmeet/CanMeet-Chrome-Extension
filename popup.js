d69f14da4d73ab65fd20ec970e117ee5b1a26cce
Xiaoming <84471360+WowShiba@users.noreply.github.com>
2022-06-05 00:14:16 +0800

    Add menu (#2)
    Co-authored-by: Xiaoming <oopsdxm@gmail.com>

--- popup.js
+++ popup.js
@@ -6,27 +6,95 @@ async function getCurrentTab() {
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
+var myAuthorization = null;
+
+
+chrome.cookies.get(
+  { url: 'https://canmeet.github.io/login/', name: 'Authorization' },
+  (res) => {
+    //alert(JSON.stringify(res.value));
+    myAuthorization = res.value;
+  },
+)
+
+
+let tab = null;
+getCurrentTab().then((res) => {
+  tab = res;
+  let tabUrl = JSON.stringify([tab.url]);
+
+  if (tabUrl.substring(2, 26) === 'https://meet.google.com/' && tabUrl.substring(26, 30) !== '"]') {
+    // In Google Meet
+    // let googleMeetId = tabUrl.substring(26, 39);
+    // alert(tabUrl.substring(26, 39));
+    chrome.cookies.get(
+      { url: 'https://canmeet.github.io/login/', name: 'Authorization' },
+      (res) => {
+
+        if (res.value != null) { // logged in
+          myAuthorization = res.value;
+          window.location.replace("/inMeet/LoggedInIndex.html");
+
+
+        }
+      },
+    )
+    setTimeout(() => {
+      // not login
+      if (myAuthorization == null) {
+        alert("not login")
+      }
+    }, 390)
+
+
+
+  } else {
+    // Not in Google Meet
+    alert("false");
+    if (myAuthorization === null) {
+      // not login
+
+    } else {
+      // logged in
+
+    }
+  }
+
+})
+
+
+
 
 injectFile.addEventListener('click', async () => {
-  let tab = await getCurrentTab();
 
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['content-script.js']
-  });
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   files: ['content-script.js']
+  // });
+
+
 });
 
 function showAlert(givenName) {
-  alert(`Hello, ${givenName}`);
+  // alert(`Hello, ${givenName} `);
+
+
 }
+
+
 
 injectFunction.addEventListener('click', async () => {
   let tab = await getCurrentTab();
-
+  alert([tab.url])
   let name = 'World';
   chrome.scripting.executeScript({
-    target: {tabId: tab.id},
+    target: { tabId: tab.id },
     func: showAlert,
     args: [tab.url]
   });
 });
+
+
+
+
+
