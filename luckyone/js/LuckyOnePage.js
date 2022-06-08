b35a6beb48875392f3426fecacf0b3ae541a1ee6
Xiaoming <oopsdxm@gmail.com>
2022-06-09 01:42:52 +0800

    Update

--- luckyone/js/LuckyOnePage.js
+++ luckyone/js/LuckyOnePage.js
@@ -1,7 +1,11 @@
 
 // 網頁載入時設定照片
-window.onload = setPhoto;
-
+// window.onload = setPhoto;
+// window.onload = setLuckData;
+window.onload = ()=>{
+    setLuckData();
+    setPhoto();
+}
 
 // 設定點擊事件
 $("#btn_QA").on("click", helpClicked);
@@ -14,13 +18,13 @@ $("#btn_OrderGenerator").on("click", logoclicked);
 
 function logoclicked() {
     console.log("logo got clicked!");
-    window.location.replace("/inMeet/LoggedInIndex.html");  
+    window.location.replace("/inMeet/LoggedInIndex.html");
 }
 
 
 
 function helpClicked() {
-    window.location.href="https://candied-football-f4f.notion.site/Canmeet-Q-A-ba85f0004771463b892949841bebf919";  
+    window.location.href = "https://candied-football-f4f.notion.site/Canmeet-Q-A-ba85f0004771463b892949841bebf919";
     window.open("https://candied-football-f4f.notion.site/Canmeet-Q-A-ba85f0004771463b892949841bebf919");
 }
 
@@ -33,7 +37,6 @@ async function getCurrentTab() {
 
 // setPhoto：設定個人照片
 function setPhoto() {
-
 
     chrome.cookies.get(
         { url: 'https://canmeet.github.io/login/', name: 'Authorization' },
@@ -62,13 +65,55 @@ function setPhoto() {
 
         },
     )
-
-
 }
 
 
 
-// refreshLuckuOne: 重新產生幸運兒按鈕功能
+function setLuckData() {
+
+    getCurrentTab().then((res) => {
+        tab = res;
+        let tabUrl = JSON.stringify([tab.url]);
+        let meetId = tabUrl.substring(26, 38);
+        console.log(meetId);
+
+
+        // let test_authorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYmNAYS5jb20iLCJlbWFpbCI6ImFiY0BhLmNvbSIsImlhdCI6MTY1NDQyOTUxMX0.2oHg_B3uakU-aOsIe4BUaPEP_Gq55bJP_ZiUCK02mU4";
+        // let test_meetID = "213as";
+
+        chrome.cookies.get(
+            { url: 'https://canmeet.github.io/login/', name: 'Authorization' },
+            (res) => {
+
+
+                // 透過ajax向指定的api url發起request
+                $.ajax(`https://canmeet.herokuapp.com/v1/meet/lucky?meetId=${meetId}`, {
+                    type: 'GET',  // http method
+                    // dataType: "json",
+                    // data: JSON.stringify({  }),
+                    headers: {
+                        "Authorization": res.value
+                    },
+
+                    success: function (data, status, xhr) {
+                        // 將id=luckuser的物件的src改成接收到的圖片位置
+                        $('#luckyuser').attr('src', data.luckyUser.photo);
+                        $('#userid').html(data.luckyUser.name);
+                        $('#refresh_hint').html(data.generateTime + "&nbsp<b>" + data.generatorName + "</b>&nbsp" + "重新產生了幸運兒");
+                        $('#current_team').text(data.groupName);
+                    },
+                    error: function (e) {
+                        console.log(JSON.stringify(e))
+                    }
+                });
+
+            },
+        )
+    })
+
+}
+
+
 function refreshLuckyOne() {
 
     getCurrentTab().then((res) => {
@@ -78,8 +123,8 @@ function refreshLuckyOne() {
         console.log(meetId);
 
 
-        let test_authorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYmNAYS5jb20iLCJlbWFpbCI6ImFiY0BhLmNvbSIsImlhdCI6MTY1NDQyOTUxMX0.2oHg_B3uakU-aOsIe4BUaPEP_Gq55bJP_ZiUCK02mU4";
-        let test_meetID = "213as";
+        // let test_authorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYmNAYS5jb20iLCJlbWFpbCI6ImFiY0BhLmNvbSIsImlhdCI6MTY1NDQyOTUxMX0.2oHg_B3uakU-aOsIe4BUaPEP_Gq55bJP_ZiUCK02mU4";
+        // let test_meetID = "213as";
 
         chrome.cookies.get(
             { url: 'https://canmeet.github.io/login/', name: 'Authorization' },
@@ -87,7 +132,7 @@ function refreshLuckyOne() {
                 
                 
                 // 透過ajax向指定的api url發起request
-                $.ajax(`https://canmeet.herokuapp.com/v1/meet/lucky?meetId=${meetId}`, {
+                $.ajax(`https://canmeet.herokuapp.com/v1/meet/lucky/draw?meetId=${meetId}`, {
                     type: 'GET',  // http method
                     // dataType: "json",
                     // data: JSON.stringify({  }),
