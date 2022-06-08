5bd7fed6906248c5501f2105ffda6072b7b4c21b
Xiaoming <84471360+WowShiba@users.noreply.github.com>
2022-06-09 01:43:53 +0800

    Update (#6)
    Co-authored-by: Xiaoming <oopsdxm@gmail.com>

--- inMeet/topic/js/vote.js
+++ inMeet/topic/js/vote.js
@@ -1,18 +1,23 @@
+
 window.onload = setPhoto;
-$("#topicBtn").on("click", topicClicked);
+$(document).ready(function() {
+    $(".firstBtn").click(function(){
+        alert("美食");
+    }); 
+    $(".secondBtn").click(function(){
+        window.location.replace("./ShowResult.html");
+    }); 
+    $(".thirdBtn").click(function(){
+        alert("3");
+    }); 
+});
+
 function setPhoto() {
-
-
     chrome.cookies.get(
         { url: 'https://canmeet.github.io/login/', name: 'Authorization' },
         (res) => {
-            //alert(JSON.stringify(res.value));
-            // myAuthorization = res.value;
-
             $.ajax('https://canmeet.herokuapp.com/v1/user/info/photo', {
                 type: 'GET',  // http method
-                // dataType: "json",
-                // data: JSON.stringify({  }),
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": res.value
@@ -20,20 +25,13 @@ function setPhoto() {
 
                 success: function (data, status, xhr) {
                     // alert(JSON.stringify(data.photoUrl))
-                    $('#myPhoto').attr('src',data.photoUrl);
+                    $('#myPhoto').attr('src', data.photoUrl);
                 },
                 error: function (e) {
                     // $('#myid').html('登入失敗, Status Code: ' + e.status + ', data: ' + JSON.stringify(e.responseJSON));
                     console.log(JSON.stringify(e))
                 }
             });
-
         },
     )
-
-    
-}
-
-function topicClicked(){
-    window.location.replace("./topic/Vote.html");
 }
