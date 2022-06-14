ada47cae80011b22ebff645c8b010bfded8897a9
szu-chieh <szuchiehhuang0505@gmail.com>
2022-06-14 22:16:22 +0800

    修改js檔(未完成)

--- Team/js/TeamEdit.js
+++ Team/js/TeamEdit.js
@@ -86,6 +86,38 @@ function setPhoto() {
 
         $('#MeetID').attr('value', meetId);
     })
+    
+    chrome.cookies.get(
+        { url: 'https://canmeet.github.io/login/', name: 'Authorization' },
+        (res) => {
+            //alert(JSON.stringify(res.value));
+            // myAuthorization = res.value;
+
+            $.ajax(`https://canmeet.herokuapp.com/v1/group/info?meetId=${meetId}`, {
+                type: 'GET',  // http method
+                // dataType: "json",
+                // data: JSON.stringify({  }),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": res.value
+                },
+
+                success: function (data, status, xhr) {
+                    // alert(JSON.stringify(data.photoUrl))
+                    $('#GroupName').attr('value', data.groupId);
+                    $('#MeetID').attr('value', data.meetId);
+                    
+                },
+                error: function (e) {
+                    // $('#myid').html('登入失敗, Status Code: ' + e.status + ', data: ' + JSON.stringify(e.responseJSON));
+                    console.log(JSON.stringify(e))
+                }
+            });
+
+        },
+    )
+
+
 
 }
 
